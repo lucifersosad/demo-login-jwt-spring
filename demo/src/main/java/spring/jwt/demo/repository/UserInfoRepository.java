04b38e7e1d3dc0b7ff860348576a8f12e6550254
Lucifer <luciferxhnh@gmail.com>
2024-03-11 20:24:08 +0700

    first commit

---
+++ demo/src/main/java/spring/jwt/demo/repository/UserInfoRepository.java
@@ -0,0 +1,13 @@
+package spring.jwt.demo.repository;
+
+import spring.jwt.demo.entity.UserInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserInfoRepository extends JpaRepository<UserInfo, Integer> {
+    Optional<UserInfo> findByName(String username);
+}
+
